4e60940f201cb4fd024e770bf7de2d10e9721c92
BIGduzy <nick-b@live.nl>
2017-01-24 22:10:23 +0100

    Moved login to separate thread

--- main.py
+++ main.py
@@ -1,40 +1,75 @@
 from Database.database import DatabaseClass
 import hashlib
+import threading
+
+# TODO: IMPLEMENT NFC reader
 
 # Constants
 # TODO: Maybe ask the user for location (for demonstration purposes)
 LOCATION = 1
 
+# GLOBALS
 db = DatabaseClass()
+user = None
 
-while True:
-    email = input("Name: ")
-    pw = input("PW: ")
 
-    # TODO: Replace login with NFC reader
-    # Sha1 encryption for password
-    sha1 = hashlib.sha1()
-    sha1.update(pw.encode(encoding='UTF-8'))
-    # Check if there is a customer with given login
-    user = db.get_customer(email, sha1.hexdigest())
+# Login check
+def login_check():
+    global db
+    global user
 
-    # if there is one, login is success
-    if user:
-        log = db.get_check_in_state(user['User_ID'])
+    while True:
+        # If there is no login
+        if not user:
+            email = input('Name: ')
+            pw = input('PW: ')
 
-        # Already checked in, so we check out
-        if log and log['Is_checkin']:
-            db.check_in_out(user['User_ID'], LOCATION, False)
-            print('Bye')
+            # Sha1 encryption for password
+            sha1 = hashlib.sha1()
+            sha1.update(pw.encode(encoding='UTF-8'))
 
-        # Already checked out, so we check in
-        else:
-            db.check_in_out(user['User_ID'], LOCATION)
-            print('Welcome')
+            # Check if there is a customer with given login
+            user = db.get_customer(email, sha1.hexdigest())
 
-    # TODO: Login should be moved to a different thread and then we need to move login failed too
-    # Login failed
-    else:
-        print('Log in failed')
+            # Login failed
+            if not user:
+                print('Log in failed')
 
-    print("\n")
+            print('\n')
+
+
+# MAIN LOOP
+def main():
+    global db
+    global user
+
+    while True:
+        # If the global variable has a value, we can assume the login was a success in the other thread
+        if user:
+            print('Login success')
+            log = db.get_check_in_state(user['User_ID'])
+
+            # Already checked in, so we check out
+            if log and log['Is_checkin']:
+                db.check_in_out(user['User_ID'], LOCATION, False)
+                print('{0} checks out!'.format(user['Email']))
+
+            # Already checked out, so we check in
+            else:
+                db.check_in_out(user['User_ID'], LOCATION)
+                print('{0} checks in!'.format(user['Email']))
+
+            # Clear user to prevent multiple check_in's
+            user = None
+
+    # Close database connection
+    db.close_connection()
+
+
+# Create thread for login check
+login_thread = threading.Thread(target = login_check)
+login_thread.daemon = True # thread dies when program ends
+login_thread.start() # Start the thread
+
+# START MAIN LOOP
+main()

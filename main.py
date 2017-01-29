bad9203d8824efb664489e45817bceacc1f5548f
BIGduzy <nick-b@live.nl>
2017-01-29 21:19:13 +0100

    Moved LED's and servo to another Thread

--- main.py
+++ main.py
@@ -7,37 +7,65 @@ import threading
 # TODO: IMPLEMENT NFC reader
 
 # Constants
-# TODO: Maybe ask the user for location (for demonstration purposes)
 BLINK_TIME = 2
-LOCATION = 1
 LED_RED = 8
 LED_GREEN = 7
 SERVO_MOTOR = 25
-SERVO_MOTOR_CLOSED = float(1) / 10.0 + 2.5
-SERVO_MOTOR_OPEN = float(90) / 10.0 + 2.5
+SERVO_MOTOR_TIME = 2
+SERVO_MOTOR_CLOSED = float(90) / 10.0 + 2.5
+SERVO_MOTOR_OPEN_CLOCK_WISE = float(0) / 10.0 + 2.5
+SERVO_MOTOR_OPEN_COUNTER_CLOCK_WISE = float(180) / 10.0 + 2.5
 
 
 # GLOBALS
 db = DatabaseClass()
 user = None
+servo_run = False
+servo_clockwise = False # True = Clock wise, False = Counter clock wise
+blink_red = False
+blink_green = False
 
 
-def blink(pin):
-    start = timer()
-    end = timer()
-    GPIO.output(pin, True) # Turn light on
-    while end - start < BLINK_TIME:
-        end = timer()
+# Led thread
+def led():
+    global blink_red
+    global blink_green
 
-    GPIO.output(pin, False) # Turn light off
+    while True:
+        if blink_red or blink_green:
+            start = timer()
+            end = timer()
+            GPIO.output(LED_RED, blink_red) # Turn light on
+            GPIO.output(LED_GREEN, blink_green) # Turn light on
+            while end - start < BLINK_TIME:
+                end = timer()
+
+            GPIO.output(LED_RED, False) # Turn light off
+            GPIO.output(LED_GREEN, False) # Turn light off
+            blink_red = False
+            blink_green = False
+
+
+# Servo thread
+def servo():
+    global servo_run
+
+    while True:
+        if servo_run:
+            start = timer()
+            end = timer()
+            pwm.ChangeDutyCycle(SERVO_MOTOR_OPEN_CLOCK_WISE if servo_clockwise else SERVO_MOTOR_OPEN_COUNTER_CLOCK_WISE)
+            while end - start < SERVO_MOTOR_TIME:
+                end = timer()
+            pwm.ChangeDutyCycle(SERVO_MOTOR_CLOSED)
+            servo_run = False
 
 
 # Login check
 def login_check():
     global db
     global user
-
-    blink_on = True
+    global blink_red
 
     while True:
         # If there is no login
@@ -55,7 +83,7 @@ def login_check():
             # Login failed
             if not user:
                 print('Log in failed')
-                blink(LED_RED)
+                blink_red = True
 
             print('\n')
 
@@ -64,26 +92,66 @@ def login_check():
 def main():
     global db
     global user
+    global servo_run
+    global servo_clockwise
+    global blink_green
+
+    # SELECT LOCATION
+    location_selected = False
+    locations = db.get_locations()
+
+    print('Select a location: ')
+    for i in locations:
+        print(i['ID'], '-', i['name'])
+
+    while not location_selected:
+        location = int(input('Location ID: '))
+        for i in locations:
+            if location == i['ID']:
+                location_selected = True
+                break
+    print('\n')
+
+
+    # START THREADS
+
+    # Create thread for login check
+    login_thread = threading.Thread(target = login_check)
+    login_thread.daemon = True # thread dies when program ends
+    login_thread.start() # Start the thread
+
+    # Create thread for servo motor
+    servo_thread = threading.Thread(target = servo)
+    servo_thread.daemon = True # thread dies when program ends
+    servo_thread.start() # Start the thread
+
+    # Create thread for LED's motor
+    led_thread = threading.Thread(target = led)
+    led_thread.daemon = True # thread dies when program ends
+    led_thread.start() # Start the thread
 
     while True:
         # If the global variable has a value, we can assume the login was a success in the other thread
         if user:
             print('Login success')
-            # TODO: Move to thread?
-            blink(LED_GREEN)
+            blink_green = True
 
             log = db.get_check_in_state(user['User_ID'])
 
             # Already checked in, so we check out
             if log and log['Is_checkin']:
-                db.check_in_out(user['User_ID'], LOCATION, False)
-                pwm.ChangeDutyCycle(SERVO_MOTOR_OPEN)
+                db.check_in_out(user['User_ID'], location, False)
+                # Open gate clockwise
+                servo_clockwise = True
+                servo_run = True
                 print('{0} checks out!'.format(user['Email']))
 
             # Already checked out, so we check in
             else:
-                db.check_in_out(user['User_ID'], LOCATION)
-                pwm.ChangeDutyCycle(SERVO_MOTOR_CLOSED)
+                db.check_in_out(user['User_ID'], location)
+                # Open gate counter-clockwise
+                servo_clockwise = False
+                servo_run = True
                 print('{0} checks in!'.format(user['Email']))
 
             # Clear user to prevent multiple check_in's
@@ -104,18 +172,16 @@ GPIO.setwarnings(False)
 GPIO.setup(LED_RED, GPIO.OUT, initial = False)
 GPIO.setup(LED_GREEN, GPIO.OUT, initial = False)
 GPIO.setup(SERVO_MOTOR, GPIO.OUT, initial = False)
-
 pwm = GPIO.PWM(SERVO_MOTOR, 100)
-pwm.start(5)
+pwm.start(SERVO_MOTOR_CLOSED)
 
-# Create thread for login check
-login_thread = threading.Thread(target = login_check)
-login_thread.daemon = True # thread dies when program ends
-login_thread.start() # Start the thread
+# pwm.ChangeDutyCycle(SERVO_MOTOR_CLOSED) # Start servo in closed state
 
-# START MAIN LOOP
-main()
-
-GPIO.cleanup()
-# Close gate
-pwm.ChangeDutyCycle(SERVO_MOTOR_CLOSED)
+try:
+    # START MAIN LOOP
+    main()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    # Close gate
+    pwm.ChangeDutyCycle(SERVO_MOTOR_CLOSED)
+    print('Program exit \n')
